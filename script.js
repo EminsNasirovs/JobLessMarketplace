faeb3a620e9373645fb52a2adb4466e9180e281f
Emins Nasirovs <116510349+EminsNasirovs@users.noreply.github.com>
2024-09-26 13:56:17 +0000

    dropdown

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ if (document.body.classList.contains('dark-mode')) {
     toggleButton.textContent = 'Dark Mode';
 }
 });
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
